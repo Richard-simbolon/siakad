35825b0ed84928303bee641c087c58a328872a4f
ctagust <agust@cashtree.id>
2019-11-13 19:55:43 +0700

    login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,12 +12,12 @@
                     <div class="kt-login__container">
                         <div class="kt-login__logo">
                             <a href="#">
-                                <img src="assets/media/logos/logo-5.png">
+                                <img src="https://polbangtanmedan.ac.id/upload/pagesdir/page_a_2019629122941.png">
                             </a>
                         </div>
                         <div class="kt-login__signin">
                             <div class="kt-login__head">
-                                <h3 class="kt-login__title">Sign In To Admin</h3>
+                                <h3 class="kt-login__title">SIAPDUDIK <br/> (Sistem Aplikasi Terpadu Pendidikan)</h3>
                             </div>
                             <form class="kt-form" method="POST" action="{{ route('login') }}">
                                 @csrf
@@ -47,73 +47,73 @@
                                             <span></span>
                                         </label>
                                     </div>-->
-                                    <div class="col kt-align-right">
-                                        <a href="javascript:;" id="kt_login_forgot" class="kt-login__link">Forget Password ?</a>
-                                    </div>
+                                    {{--<div class="col kt-align-right">--}}
+                                        {{--<a href="javascript:;" id="kt_login_forgot" class="kt-login__link">Forget Password ?</a>--}}
+                                    {{--</div>--}}
                                 </div>
                                 <div class="kt-login__actions">
                                         <button  type="submit" class="btn btn-brand btn-elevate kt-login__btn-primary">
-                                            {{ __('Login') }}
+                                            <h5>{{ __('Masuk') }}</h5>
                                         </button>
                                            
                                 </div>
                             </form>
                         </div>
-                        <div class="kt-login__signup">
-                            <div class="kt-login__head">
-                                <h3 class="kt-login__title">Sign Up</h3>
-                                <div class="kt-login__desc">Enter your details to create your account:</div>
-                            </div>
-                            <form class="kt-form" action="">
-                                <div class="input-group">
-                                    <input class="form-control" type="text" placeholder="Fullname" name="fullname">
-                                </div>
-                                <div class="input-group">
-                                    <input class="form-control" type="text" placeholder="Email" name="email" autocomplete="off">
-                                </div>
-                                <div class="input-group">
-                                    <input class="form-control" type="password" placeholder="Password" name="password">
-                                </div>
-                                <div class="input-group">
-                                    <input class="form-control" type="password" placeholder="Confirm Password" name="rpassword">
-                                </div>
-                                <div class="row kt-login__extra">
-                                    <div class="col kt-align-left">
-                                        <label class="kt-checkbox">
-                                            <input type="checkbox" name="agree">I Agree the <a href="#" class="kt-link kt-login__link kt-font-bold">terms and conditions</a>.
-                                            <span></span>
-                                        </label>
-                                        <span class="form-text text-muted"></span>
-                                    </div>
-                                </div>
-                                <div class="kt-login__actions">
-                                    <button id="kt_login_signup_submit" class="btn btn-brand btn-elevate kt-login__btn-primary">Sign Up</button>&nbsp;&nbsp;
-                                    <button id="kt_login_signup_cancel" class="btn btn-light btn-elevate kt-login__btn-secondary">Cancel</button>
-                                </div>
-                            </form>
-                        </div>
-                        <div class="kt-login__forgot">
-                            <div class="kt-login__head">
-                                <h3 class="kt-login__title">Forgotten Password ?</h3>
-                                <div class="kt-login__desc">Enter your email to reset your password:</div>
-                            </div>
-                            <form class="kt-form" action="">
-                                <div class="input-group">
-                                    <input class="form-control" type="text" placeholder="Email" name="email" id="kt_email" autocomplete="off">
-                                </div>
-                                <div class="kt-login__actions">
-                                    <button id="kt_login_forgot_submit" class="btn btn-brand btn-elevate kt-login__btn-primary">Request</button>&nbsp;&nbsp;
-                                    <button id="kt_login_forgot_cancel" class="btn btn-light btn-elevate kt-login__btn-secondary">Cancel</button>
-                                </div>
-                            </form>
-                        </div>
-                        <div class="kt-login__account">
-                            <span class="kt-login__account-msg">
-                                Don't have an account yet ?
-                            </span>
-                            &nbsp;&nbsp;
-                            <a href="javascript:;" id="kt_login_signup" class="kt-login__account-link">Sign Up!</a>
-                        </div>
+                        {{--<div class="kt-login__signup">--}}
+                            {{--<div class="kt-login__head">--}}
+                                {{--<h3 class="kt-login__title">Sign Up</h3>--}}
+                                {{--<div class="kt-login__desc">Enter your details to create your account:</div>--}}
+                            {{--</div>--}}
+                            {{--<form class="kt-form" action="">--}}
+                                {{--<div class="input-group">--}}
+                                    {{--<input class="form-control" type="text" placeholder="Fullname" name="fullname">--}}
+                                {{--</div>--}}
+                                {{--<div class="input-group">--}}
+                                    {{--<input class="form-control" type="text" placeholder="Email" name="email" autocomplete="off">--}}
+                                {{--</div>--}}
+                                {{--<div class="input-group">--}}
+                                    {{--<input class="form-control" type="password" placeholder="Password" name="password">--}}
+                                {{--</div>--}}
+                                {{--<div class="input-group">--}}
+                                    {{--<input class="form-control" type="password" placeholder="Confirm Password" name="rpassword">--}}
+                                {{--</div>--}}
+                                {{--<div class="row kt-login__extra">--}}
+                                    {{--<div class="col kt-align-left">--}}
+                                        {{--<label class="kt-checkbox">--}}
+                                            {{--<input type="checkbox" name="agree">I Agree the <a href="#" class="kt-link kt-login__link kt-font-bold">terms and conditions</a>.--}}
+                                            {{--<span></span>--}}
+                                        {{--</label>--}}
+                                        {{--<span class="form-text text-muted"></span>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                                {{--<div class="kt-login__actions">--}}
+                                    {{--<button id="kt_login_signup_submit" class="btn btn-brand btn-elevate kt-login__btn-primary">Sign Up</button>&nbsp;&nbsp;--}}
+                                    {{--<button id="kt_login_signup_cancel" class="btn btn-light btn-elevate kt-login__btn-secondary">Cancel</button>--}}
+                                {{--</div>--}}
+                            {{--</form>--}}
+                        {{--</div>--}}
+                        {{--<div class="kt-login__forgot">--}}
+                            {{--<div class="kt-login__head">--}}
+                                {{--<h3 class="kt-login__title">Forgotten Password ?</h3>--}}
+                                {{--<div class="kt-login__desc">Enter your email to reset your password:</div>--}}
+                            {{--</div>--}}
+                            {{--<form class="kt-form" action="">--}}
+                                {{--<div class="input-group">--}}
+                                    {{--<input class="form-control" type="text" placeholder="Email" name="email" id="kt_email" autocomplete="off">--}}
+                                {{--</div>--}}
+                                {{--<div class="kt-login__actions">--}}
+                                    {{--<button id="kt_login_forgot_submit" class="btn btn-brand btn-elevate kt-login__btn-primary">Request</button>&nbsp;&nbsp;--}}
+                                    {{--<button id="kt_login_forgot_cancel" class="btn btn-light btn-elevate kt-login__btn-secondary">Cancel</button>--}}
+                                {{--</div>--}}
+                            {{--</form>--}}
+                        {{--</div>--}}
+                        {{--<div class="kt-login__account">--}}
+                            {{--<span class="kt-login__account-msg">--}}
+                                {{--Don't have an account yet ?--}}
+                            {{--</span>--}}
+                            {{--&nbsp;&nbsp;--}}
+                            {{--<a href="javascript:;" id="kt_login_signup" class="kt-login__account-link">Sign Up!</a>--}}
+                        {{--</div>--}}
                     </div>
                 </div>
             </div>
